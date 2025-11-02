cb20939be42c5221ab5abffbf094d86fb40da8b2
Lironktf <Lironktf@gmail.com>
2025-11-01 21:42:24 -0400

    Initial Commit

---
+++ MacAppSwitcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacAppSwitcher
+//
+//  Created by Liron Katsif on 2025-11-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacAppSwitcher/MacAppSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacAppSwitcherApp.swift
+//  MacAppSwitcher
+//
+//  Created by Liron Katsif on 2025-11-01.
+//
+
+import SwiftUI
+
+@main
+struct MacAppSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
